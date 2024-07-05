533e0d50ebe585491de1d11cd4711a6c3a23b0e5
OlegShaldybin98 <oleg.shaldybin10@gmail.com>
2024-07-05 17:50:29 +0300

    Add files via upload

---
+++ задание 1.py
@@ -0,0 +1,12 @@
+#"1st program"
+print(9**0.5*5)
+#"2nd program"
+print(9.99>9.98 and 1000!=1000.1)
+#"3rd program"
+print((123%100) + (567%100))
+#"4th program"
+a, b = 13.42, 42.13
+ai, bi = int(a), int(b)
+af, bf = int((a - ai) * 100), int((b - bi) * 100)
+print(ai == bf or bi == af)
+
