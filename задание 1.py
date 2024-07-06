bcac02c0d07e0c2622eda0009ffa0099faadc2ee
OlegShaldybin98 <oleg.shaldybin10@gmail.com>
2024-07-06 23:43:16 +0300

    Add files via upload

--- задание 1.py
+++ задание 1.py
@@ -3,7 +3,7 @@ print(9**0.5*5)
 #"2nd program"
 print(9.99>9.98 and 1000!=1000.1)
 #"3rd program"
-print((123%100) + (567%100))
+print(((1234//10)%100) + ((5678//10)%100))
 #"4th program"
 a, b = 13.42, 42.13
 ai, bi = int(a), int(b)
