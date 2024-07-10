866680942db1d20f2504428d1adfc0983b86e3aa
OlegShaldybin98 <oleg.shaldybin10@gmail.com>
2024-07-10 15:46:10 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+count_homeworks = 12
+time_for_homework = 1.5
+course_name = "Python,"
+time_for_1_HW = (time_for_homework/count_homeworks)
+print("Курс:",course_name,"Всего задач:",count_homeworks,", Затрачено часов:",time_for_homework,", Среднее время выполнения:",time_for_1_HW,".")
