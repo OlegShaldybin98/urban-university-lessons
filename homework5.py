4a92a22d53f125ddd7e1570bcd62112b1a5caff3
OlegShaldybin98 <oleg.shaldybin10@gmail.com>
2024-07-12 15:28:53 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,8 @@
+immutable_var = (1, "string", True)
+print("immutable_var:", immutable_var)
+#immutable_var[0] = 2
+#print(immutable_var)
+mutable_list = [1, "string", True]
+mutable_list [0] = 2
+mutable_list [1] = "list"
+print("mutable_list: ", mutable_list)
